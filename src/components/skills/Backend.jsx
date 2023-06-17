8fb3f1ff32f853c8e8ee77c4abfa78abaf36988c
KamilFilipczak <k.filipczak@icloud.com>
2023-06-17 16:11:48 +0200

    skills update

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -15,13 +15,8 @@ const Backend = () => {
                         <span className="skills__level">Basic</span>
                     </div>
                 </div>
-                <div className="skills__data">
-                    <HiCheckBadge />
-                    <div>
-                        <h3 className="skills__name">Node.js</h3>
-                        <span className="skills__level">Basic</span>
-                    </div>
-                </div>
+
+
                 <div className="skills__data">
                     <HiCheckBadge />
                     <div>
